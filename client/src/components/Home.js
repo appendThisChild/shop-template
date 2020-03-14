c9ae7c50484f51b8da4c1480c03d84d7529e0a69
Ryan Pettingill <pettingillryan@gmail.com>
2020-03-14 12:19:17 -0600

    turn scroll-snap feature into their own components for multi-use, moving on to notes and design ideas

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,52 +1,51 @@
-import React, { useRef } from 'react'
-import styled from 'styled-components'
+import React, { useEffect} from 'react'
+// import styled from 'styled-components'
 // import gsap from 'gsap'
 
 // components
-import HomeSection from "./HomeSection.js"
+import ScrollSnap from './ScrollSnap.js'
+import ScrollSnapSection from "./ScrollSnapSection.js"
 import HomeTop from './HomeTop.js'
 
-const HomeStyle = styled.div`
-    height: 100vh;
-    overflow: scroll;
-    scroll-snap-type: y mandatory;
 
-    > div {
-        height: 100vh;
-        scroll-snap-align: center;
-        scroll-snap-stop: always;
-    }
-`
 
 const Home = () => {
-    let containerElement = useRef(null)
-    const scrollUpDown = height => containerElement.scrollTo({ top: containerElement.scrollTop + height, behavior: "smooth" });
 
-    // useEffect(() => {
-    //     console.log(containerElement.getBoundingClientRect())
-    // }, [])
+    useEffect(() => {
+        // document.getElementById("cocks").scrollIntoView()
+    }, [])
     
     return(
-        <HomeStyle ref={el => containerElement = el}>
-            <HomeSection scroll={scrollUpDown} noTop={true}>
+        <ScrollSnap vertical={true}>
+            <ScrollSnapSection>
                 <HomeTop />
-            </HomeSection>
-            <HomeSection scroll={scrollUpDown}>
+                
+            </ScrollSnapSection>
+            <ScrollSnapSection>
                 <main>
                     Main
                 </main>
-            </HomeSection>
-            <HomeSection scroll={scrollUpDown}>
-                <section>
-                    Section
-                </section>
-            </HomeSection>
-            <HomeSection scroll={scrollUpDown} noBottom={true}>
+            </ScrollSnapSection>
+            <ScrollSnapSection>
+                <ScrollSnap>
+                    <ScrollSnapSection>
+                    <section>
+                        Section
+                    </section>
+                    </ScrollSnapSection>
+                    <ScrollSnapSection>
+                    <section id="cocks">
+                        Section
+                    </section>
+                    </ScrollSnapSection>
+                </ScrollSnap>
+            </ScrollSnapSection>
+            <ScrollSnapSection>
                 <footer>
                     Bottom
                 </footer>
-            </HomeSection>
-        </HomeStyle>
+            </ScrollSnapSection>
+        </ScrollSnap>
     )
 }
 
---
+++ client/src/components/ScrollSnapSection.js
@@ -0,0 +1,84 @@
+import React, { useRef } from 'react'
+import styled from 'styled-components'
+
+
+const SectionStyle = styled.div`
+    > aside {
+        display: flex;  
+    }
+    > aside:first-child {
+        margin-left: ${({ vertical }) => vertical ? "auto" : ""};
+        margin-right: auto;
+        grid-column: 1/-1;
+        grid-row: ${({ vertical }) => vertical ? "1/2" : "5/6"};
+        align-items: ${({ vertical }) => vertical ? "flex-start" : "center"};
+    }
+    > aside:last-child {
+        margin-left: auto;
+        margin-right: ${({ vertical }) => vertical ? "auto" : ""};
+        grid-column: 1/-1;
+        grid-row: ${({ vertical }) => vertical ? "10/-1" : "5/6"};
+        align-items: ${({ vertical }) => vertical ? "flex-end" : "center"};
+    }
+    > aside > button {
+        height: 3em;
+        width: 3em;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: 0;
+        background-color: transparent;
+    }
+    > aside > button > span {
+        display: block;
+        height: 1.5em;
+        width: 1.5em;
+        border-top-left-radius: .3em;
+        border-bottom-right-radius: .3em;
+
+        border-top: .3em solid rgba(0,0,0,.5);
+        border-right: .3em solid rgba(0,0,0,.5);
+    }
+    > div {
+        grid-row: 1/-1;
+        grid-column: 1/-1;
+    }
+`
+const ButtonStyle1 = styled.button`
+    margin-top: ${({ vertical }) => vertical ? ".75em" : ""};
+    margin-left: ${({ vertical }) => vertical ? "" : ".75em"};
+`
+const ButtonStyle2 = styled.button`
+    margin-bottom: ${({ vertical }) => vertical ? ".75em" : ""};
+    margin-right: ${({ vertical }) => vertical ? "" : ".75em"};
+`
+
+const ScrollSnapSection = ({ noBack, noForward, vertical, scroll, children }) => {
+    let containerRef = useRef(null)
+    const distanceOfElement = el => vertical ? el.getBoundingClientRect().height : el.getBoundingClientRect().width;
+    const angle = vertical ? 135 : 45
+    
+    return(
+        <SectionStyle ref={el => containerRef = el} vertical={vertical}>
+            {!noBack ?
+                <aside>
+                    <ButtonStyle1 onClick={() => scroll(-distanceOfElement(containerRef))} vertical={vertical}>
+                        <span style={{ transform: `rotate(${angle + 180}deg)`}}></span>
+                    </ButtonStyle1>
+                </aside>
+            :null}
+            <div>
+                {children}
+            </div>
+            {!noForward ?
+                <aside>
+                    <ButtonStyle2 onClick={() => scroll(distanceOfElement(containerRef))} vertical={vertical}>
+                        <span style={{ transform: `rotate(${angle}deg)`}}></span>
+                    </ButtonStyle2>
+                </aside>
+            :null}
+        </SectionStyle>
+    )
+}
+
+export default ScrollSnapSection;
